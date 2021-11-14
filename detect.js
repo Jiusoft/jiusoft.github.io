5271b728423fcb12dd4f4a330d4ab4540337930e
B.Jothin kumar <bjothinphysics@gmail.com>
2021-11-14 14:12:02 +0530

    Fix errors

--- detect.js
+++
@@ -1,7 +0,0 @@
-function makeSureUserIsInDesktop() {
-  const isMobile = (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.platform))); //resolves true/false
-  if (isMobile == true) {
-    document.getElementById("body").innerHTML = "<p>Sorry, mobile devices are not supported...</p>";
-  }
-}
-makeSureUserIsInDesktop();
