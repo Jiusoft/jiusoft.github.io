4a5ff8e078434c6a28c16247b09af646c0ca738e
Orlando Huang <86908394+orlandohuang@users.noreply.github.com>
2021-11-14 15:23:06 +0700

    Update detect.js

--- detect.js
+++ detect.js
@@ -1,5 +1,5 @@
 function makeSureUserIsInDesktop() {
-  const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.platform))); //resolves true/false
+  const isMobile = (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) || (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.platform))); //resolves true/false
   if (isMobile == true) {
     document.getElementById("body").innerHTML = "<p>Sorry, mobile devices are not supported...</p>";
   }
