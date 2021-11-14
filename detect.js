239c8b02902e3378f67c0dbbda2500557f682c98
Orlando Huang <orlando.huang8@pm.me>
2021-11-14 15:52:09 +0700

    *

--- detect.js
+++
@@ -1,4 +0,0 @@
-var isMobile = /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent);
-if (isMobile) {
-    document.getElementById("body").innerHTML = "<p>Sorry, mobile devices are not supported...</p>";
-}
